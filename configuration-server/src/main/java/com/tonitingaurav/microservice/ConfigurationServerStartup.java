ec54357ae85db6a44602efbe013cb151ef3a1aef
tonitingaurav <tonitingaurav@gmail.com>
2020-09-24 19:13:25 +0530

    Encrypt the password

--- configuration-server/src/main/java/com/tonitingaurav/microservice/ConfigurationServerStartup.java
+++ configuration-server/src/main/java/com/tonitingaurav/microservice/ConfigurationServerStartup.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@EnableEncryptableProperties
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigurationServerStartup {
