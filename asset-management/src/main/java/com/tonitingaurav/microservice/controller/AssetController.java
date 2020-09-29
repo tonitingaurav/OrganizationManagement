0fb5c766c44541ed59b20646e9df44c3b04f1d63
tonitingaurav <tonitingaurav@gmail.com>
2020-09-29 17:19:45 +0530

    Remove context

--- asset-management/src/main/java/com/tonitingaurav/microservice/controller/AssetController.java
+++ asset-management/src/main/java/com/tonitingaurav/microservice/controller/AssetController.java
@@ -22,7 +22,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/asset")
 @Api(value = "Asset CURD Operations", tags = { "asset" })
 public class AssetController {
 
