4999ec5418ae817440cca6e7d442b4ae6cb0bb92
tonitingaurav <tonitingaurav@gmail.com>
2020-09-29 18:34:30 +0530

    Integrate ribbon with Eureka

--- org-orchestration/src/main/java/com/tonitingaurav/microservice/client/emp/EmployeeAPIServiceProxy.java
+++ org-orchestration/src/main/java/com/tonitingaurav/microservice/client/emp/EmployeeAPIServiceProxy.java
@@ -1,13 +1,16 @@
 package com.tonitingaurav.microservice.client.emp;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.tonitingaurav.microservice.model.Employees;
 
-@FeignClient(name = "employee-management", url = "localhost:8080")
+//@FeignClient(name = "employee-management", url = "localhost:8080")
+@FeignClient(name = "employee-management")
+@RibbonClient(name = "employee-management")
 public interface EmployeeAPIServiceProxy {
-	
+
 	@GetMapping("/employee")
 	public Employees getAllEmployees();
 
