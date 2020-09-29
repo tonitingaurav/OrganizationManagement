5d79495db2b6c7860c0dfc56fea1954ada3faf36
tonitingaurav <tonitingaurav@gmail.com>
2020-09-29 21:28:02 +0530

    use zuul

--- org-orchestration/src/main/java/com/tonitingaurav/microservice/client/emp/EmployeeAPIServiceProxy.java
+++ org-orchestration/src/main/java/com/tonitingaurav/microservice/client/emp/EmployeeAPIServiceProxy.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.tonitingaurav.microservice.model.Employees;
 
 //@FeignClient(name = "employee-management", url = "localhost:8080")
-@FeignClient(name = "employee-management")
+//@FeignClient(name = "employee-management") //used in absence of zuul
+@FeignClient(name = "zuul-api-gateway")
 @RibbonClient(name = "employee-management")
 public interface EmployeeAPIServiceProxy {
 
-	@GetMapping("/employee")
+	// @GetMapping("/employee") //used in absence of zuul
+	@GetMapping("/employee-management/employee")
 	public Employees getAllEmployees();
 
 }
