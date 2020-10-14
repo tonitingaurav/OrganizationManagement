5c5b7c542a388074dc4e3f5f30267b7db7eda7ed
tonitingaurav <tonitingaurav@gmail.com>
2020-10-14 13:49:39 +0530

    Add Spring profiling

--- configuration-server/src/main/java/com/tonitingaurav/microservice/ConfigurationServerStartup.java
+++ configuration-server/src/main/java/com/tonitingaurav/microservice/ConfigurationServerStartup.java
@@ -2,12 +2,14 @@ package com.tonitingaurav.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @EnableEncryptableProperties
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigurationServerStartup {
 
